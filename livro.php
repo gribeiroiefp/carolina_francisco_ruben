e647c7c22e20edaa2c14ff0e156b400d03a8d5c9
RubenMont90 <rubenmont.90@gmail.com>
2025-09-15 16:25:25 +0100

    deassociate authors from book, in livro.php

--- livro.php
+++ livro.php
@@ -7,24 +7,19 @@ if (!$conn) {
 
 $id = (int) $_GET['id'];
 
-// // Handle deletion of the movie
-// if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_movie'])) {
-//     mysqli_query($conn, "DELETE FROM filmes WHERE id = $id");
-//     header('Location: index.php');
-//     exit;
-// }
 
-// // Handle adding an actor to this movie
-// if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_actor'])) {
-//     $id_ator = (int) $_POST['id_ator'];
-//     $personagem = mysqli_real_escape_string($conn, $_POST['personagem']);
+// Handle deassociate author from this book
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deassociate_author'])) {
+    $id_author = (int) $_POST['id_author'];
 
-//     if ($id_ator > 0 && $personagem !== '') {
-//         $sql_insert = "INSERT INTO filme_ator (id_filme, id_ator, personagem) 
-//                        VALUES ($id, $id_ator, '$personagem')";
-//         mysqli_query($conn, $sql_insert);
-//     }
-// }
+    // echo "autor>> " . $id_author . " livro>> " . $id;
+
+    if ($id_author > 0){
+        mysqli_query($conn, "DELETE FROM autores_livros WHERE id_autor = $id_author AND id_livro = $id");
+        header('Location: livro.php?id=' . $id);
+        exit();
+    }
+}
 
 // Fetch book info
 $sql = "SELECT * FROM livros WHERE id = $id";
@@ -56,10 +51,12 @@ $resultado_todos_autores = mysqli_query($conn, $sql_todos_autores);
 // Handle adding an author to this book
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_author'])) {
     $id_autor = (int) $_POST['id_author'];
-    echo $id_autor;
+    //echo $id_autor;
     if ($id_autor > 0) {
         $sql_insert = "INSERT INTO autores_livros (id_livro, id_autor) VALUES ($id, $id_autor)";
         mysqli_query($conn, $sql_insert);
+        header('Location: livro.php?id=' . $id);
+        exit();
     }
 }
 
@@ -113,24 +110,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_author'])) {
                         $foto = htmlspecialchars($autor['foto']);
                         $nacionalidade = htmlspecialchars($autor['nacionalidade']);
                         echo <<<HTML
-                    <a href="autor.php?id=$id_autor" class="row align-items-end mb-2 author-item">
-                        <img src="$foto" alt="$nome" class="col-2">
-                        <div class="col-9">
-                            <p class="nome mb-0">$nome</p>
-                            <p class="nacionalidade">$nacionalidade</p>
-                        </div>
-                        <div class="col-1 align-self-start">
-                            <!-- <form method="POST" onsubmit="return confirm('Tem certeza que deseja desassociar este autor a este livro?')">
-                                <button type="submit" class="btn btn-danger btn-remover">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-                                        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
-                                    </svg>
-                                </button>
-                            </form> -->
-                        </div>
-                    </a>
-                    HTML;
-                    }
+                        <a href="autor.php?id=$id_autor" class="row align-items-end mb-2 author-item">
+                            <img src="$foto" alt="$nome" class="col-2">
+                            <div class="col-9">
+                                <p class="nome mb-0">$nome</p>
+                                <p class="nacionalidade">$nacionalidade</p>
+                            </div>
+                            <div class="col-1 align-self-start">
+                                <form method="POST" onsubmit="return confirm('Tem certeza que deseja desassociar este autor a este livro?')">
+                                    <input type="hidden" name="id_author" value="$id_autor">
+                                    <button type="submit" name="deassociate_author" class="btn btn-danger btn-remover">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                                        </svg>
+                                    </button>
+                                </form>
+                            </div>
+                        </a>
+                        HTML;
+                        }
                 } else {
                     echo "<p>Nenhum autor associado a este livro.</p>";
                 }
