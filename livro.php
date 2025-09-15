4a985a738e8428038ef3e2d5fa99f12d558d652a
RubenMont90 <rubenmont.90@gmail.com>
2025-09-15 14:06:56 +0100

    commented button to deassociate author from book

--- livro.php
+++ livro.php
@@ -91,7 +91,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_author'])) {
         </div>
     </header>
 
-<br>
+    <br>
     <div class="container-lg filme">
         <div class="row align-items-center info">
             <img src="<?php echo htmlspecialchars($livro['capa']); ?>" alt="" class="col-3">
@@ -99,23 +99,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_author'])) {
                 <h2><?php echo htmlspecialchars($livro['titulo']); ?></h2>
                 <p><span class="rotulo ano">Ano:</span> <?php echo htmlspecialchars($livro['ano']); ?></p>
             </div>
-            <!-- <div class="col-1 opcoes align-self-start">
-                <a href="editar_filme.php?id=<? //= $livro['id'] 
-                                                ?>" class="btn btn-primary btn-editar">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
-                    </svg>
-                </a>
-                <form method="post" style="display:inline;" onsubmit="return confirm('Tem certeza que deseja apagar este filme?');">
-                    <input type="hidden" name="delete_movie" value="1">
-                    <button type="submit" class="btn btn-danger btn-remover"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-                            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
-                        </svg></button>
-                </form>
-            </div> -->
         </div>
 
-<br>
+        <br>
         <div class="informacao">
             <div class="lista col">
                 <h3>Autores</h3>
@@ -129,9 +115,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_author'])) {
                         echo <<<HTML
                     <a href="autor.php?id=$id_autor" class="row align-items-end mb-2 author-item">
                         <img src="$foto" alt="$nome" class="col-2">
-                        <div class="col-10">
+                        <div class="col-9">
                             <p class="nome mb-0">$nome</p>
                             <p class="nacionalidade">$nacionalidade</p>
+                        </div>
+                        <div class="col-1 align-self-start">
+                            <!-- <form method="POST" onsubmit="return confirm('Tem certeza que deseja desassociar este autor a este livro?')">
+                                <button type="submit" class="btn btn-danger btn-remover">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                                    </svg>
+                                </button>
+                            </form> -->
                         </div>
                     </a>
                     HTML;
